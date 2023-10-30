efd6275990344bbe38faca11e3ee7f41859d29c7
Spiletto <marselinusdira@gmail.com>
2023-10-30 10:05:21 +0700

    first commit

---
+++ resources/views/movie/edit.blade.php
@@ -0,0 +1,72 @@
+@extends('dashboard')
+@section('content')
+<div class="content-header">
+    <div class="container-fluid"> 
+        <div class="row mb-2"> 
+            <div class="col-sm-6"> 
+                <h1 class="m-0">Edit Movie</h1> 
+            </div>
+            <!-- /.col --> 
+            <div class="col-sm-6"> 
+                <ol class="breadcrumb float-sm-right"> 
+                    <li class="breadcrumb-item"> 
+                        <a href="#">Movie</a>
+                    </li>
+                    <li class="breadcrumb-item active">Edit</li>
+                </ol>
+            </div> 
+            <!-- /.col --> 
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div> 
+<!-- /.content-header --> 
+<!-- Main content -->
+<div class="content"> 
+    <div class="container-fluid"> 
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{ route('movie.update', $movie->id)}}" method="POST" enctype="multipart/form-data">
+                            @csrf 
+                            @method('PUT') 
+                            <div class="form-row"> 
+                                <div class="form-group col-md-12">
+                                    <label class="font-weightbold">Title</label> 
+                                    <input type="text" class="form-control
+                                    @error('title') is-invalid @enderror" name="title" value="{{ old('title',
+                                    $movie->title )}}">
+                                    @error('title')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label class="font-weightbold">Director</label>
+                                    <input type="text" class="form-control
+                                    @error('director') is-invalid @enderror" name="director" value="{{
+                                    old('director', $movie->director) }}">
+                                    @error('director')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-md btnprimary">SIMPAN</button>
+                        </form>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div>
+@endsection
